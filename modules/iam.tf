d781ef74e56ff358233770091a2ff093a6507ad7
adamgft <adam.crow@gft.com>
2021-01-14 14:21:26 +0000

    enable folder audit

--- modules/iam.tf
+++ modules/iam.tf
@@ -81,7 +81,7 @@ module "project-services" {
   project_id    = var.project_id
   activate_apis = var.project_apis
 }
-/*
+
 ### need to fork and update to include audit on folder
 ### https://github.com/terraform-google-modules/terraform-google-iam/tree/1df0f6a6a385ba92e831937ee9772a2c5508e302/modules/audit_config
 resource "google_folder_iam_audit_config" "audit_logging" {
@@ -92,4 +92,3 @@ resource "google_folder_iam_audit_config" "audit_logging" {
   }
 }
 
-*/
