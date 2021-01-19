b1836006e861a12bb4b18544b5366a8fae4a92eb
e-amce <adam.crow@gft.com>
2021-01-19 14:00:33 +0000

    fixed output names

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vpc_id" {
-  value = module.vpc.network
+  value = lookup(module.vpc.network, "id", "noid")
 }
 
-output "subnet-lookup-element-values" {
+output "subnet_id" {
   value = lookup(element(values(module.subnets.subnets),0), "id", "noid" )
 }
